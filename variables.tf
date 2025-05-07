71a6625c5b79e38312ce132f88ed2023cd864a2c
Viktor Ribchev <viktor.ribchev@ontotext.com>
2025-05-07 15:18:18 +0300

    Updated GDB to version 11.0.0
    (cherry picked from commit ab21d07b2369c0df2c9d9fa13f7aa9eb7e6c37ad)

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "source_image" {
   type        = string
   # Set the default value to your image. Marketplace will overwrite this value
   # to a Marketplace owned image on publishing the product
-  default = "projects/graphdb-public/global/images/ontotext-graphdb-10-8-5-202504011625"
+  default = "projects/graphdb-public/global/images/ontotext-graphdb-11-0-0-202504080820"
 }
 
 variable "goog_cm_deployment_name" {
